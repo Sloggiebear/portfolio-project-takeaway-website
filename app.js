a768325d130f5cadcf3c5e851f46a80b2ebfd424
Logan McLain <Sloggiebear@users.noreply.github.com>
2022-01-21 15:28:31 +0000

    initial build

---
+++ app.js
@@ -0,0 +1,135 @@
+
+const apiURL = "https://api.npoint.io/5f458ccb947908d10993"
+
+const formatter = new Intl.NumberFormat('en-EU', {
+    style: 'currency',
+    currency: 'EUR',
+    minimumFractionDigits: 2
+})
+
+async function getData() {
+    try {
+        const response = await fetch(apiURL, { method: 'GET' })
+        const parsed = await response.json()
+        console.table(parsed)
+        addNavLinks(parsed)
+        addMenuSections(parsed)
+    } catch (error) {
+        console.log("Connection Error", error)
+    }
+}
+
+getData()
+
+function addNavLinks(data) {
+    let navitems = document.getElementById("navitems")
+    for (let i = 0; i < data.MenuSections.length; i++) {
+        let el = document.createElement("li")
+        el.innerHTML = `<a href="#${data.MenuSections[i].Name}">${data.MenuSections[i].Name}</a>`
+        navitems.appendChild(el)
+    }
+}
+
+function addMenuSections(data) {
+    let menu = document.getElementById("menu")
+    for (let i = 0; i < data.MenuSections.length; i++) {
+
+        let section = document.createElement("div")
+        section.classList.add("menu--section")
+        section.id = data.MenuSections[i].Name
+        menu.appendChild(section)
+
+        let heading = document.createElement("h2")
+        heading.innerText = data.MenuSections[i].Name
+        section.appendChild(heading)
+
+        for (let y = 0; y < data.MenuSections[i].MenuItems.length; y++) {
+
+            let menuItem = document.createElement("div")
+            menuItem.dataset.itemName = data.MenuSections[i].MenuItems[y].Name
+            menuItem.dataset.itemPrice = data.MenuSections[i].MenuItems[y].Price
+            menuItem.classList.add("menu-item")
+            menuItem.addEventListener('click', (event) => {
+                showModal(event)
+            })
+            section.appendChild(menuItem)
+
+            let itemName = document.createElement("p")
+            itemName.innerHTML = data.MenuSections[i].MenuItems[y].Name
+            menuItem.appendChild(itemName)
+
+            let itemPrice = document.createElement("p")
+            itemPrice.innerHTML = formatter.format(data.MenuSections[i].MenuItems[y].Price)
+            menuItem.appendChild(itemPrice)
+
+            if (data.MenuSections[i].MenuItems[y].SoldOut) {
+                menuItem.classList.add("soldout")
+            }
+
+        }
+    }
+}
+
+const modal = document.getElementById('modal')
+const closeButton = document.getElementById('btn-close')
+const addToOrderButton = document.getElementById('btn-addtoorder')
+let quantity = document.getElementById('quantity')
+const quantityMinus = document.getElementById('btn-quanity-minus')
+const quantityPlus = document.getElementById('btn-quanity-plus')
+
+//onclick - Hide the modal user clicks close button
+closeButton.addEventListener('click', () => {
+    modal.classList.toggle('show')
+})
+
+//onclick - Hide the modal user clicks outside of the modal content
+modal.addEventListener('click', (event) => {
+    if (event.target.id === "modal") {
+        modal.classList.toggle('show')
+    }
+})
+
+//onclick - Hide the modal user clicks close button
+addToOrderButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    modal.classList.toggle('show')
+})
+
+//onclick - Reduce the value of the order quantity
+quantityMinus.addEventListener('click', (event) => {
+    event.preventDefault()
+    if (quantity.value == 1) {
+        return
+    }
+
+    else {
+        quantity.value -= 1
+    }
+})
+
+//onclick - Increase the value of the order quantity
+quantityPlus.addEventListener('click', (event) => {
+    event.preventDefault()
+    console.log("plus")
+    let value = parseInt(quantity.value)
+    value++
+    quantity.value = value
+})
+
+//onclick - Prevents the value of the input number to be less than 1
+quantity.addEventListener('change', (event) => {
+    if (quantity.value < 1) {
+        quantity.value = 1
+    }
+})
+
+
+function showModal(event) {
+    modal.classList.toggle('show')
+    let getItemName = event.target.getAttribute("data-item-name");
+    let getItemPrice = event.target.getAttribute("data-item-price");
+    let modaltitle = document.getElementById("addcart-item-name")
+    let modalprice = document.getElementById("addcart-item-price")
+    modaltitle.innerText = getItemName
+    modalprice.innerText = formatter.format(getItemPrice)
+}
